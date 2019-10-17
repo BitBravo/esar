71563c49d313b33c65545c509c45e7cbf00c89c6
jsdev63 <react.dev@yahoo.com>
2019-10-17 21:26:42 +0200

    prod

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -68,6 +68,9 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/contact">
                 Contact
               </Link>
+              <Link className="navbar-item" to="/products">
+                Product
+              </Link>
             </div>
             <div className="navbar-end has-text-centered">
             </div>
